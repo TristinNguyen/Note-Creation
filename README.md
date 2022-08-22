782ef5ad13bfbbf5e3f78d7a9ce981cba2b501a9
Tristin Nguyen <nguye501@msu.edu>
2022-08-22 19:30:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+
+  <h1 align="center">Cloud Note Taker</h1><br/>
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) <br/><br/>
+
+  ## Description
+🔍 A node.js application used to create, store and delete notes.<br/><br/>
+
+![Note Taker](./public/assets/images/cloud-note-taker.png)<br/>
+Deployed live on [heroku](https://salty-anchorage-58170.herokuapp.com/)<br/>
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [User Story](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Testing](#testing)
+  - [Questions](#questions)
+
+  ## Installation
+  `npm install from the project directory` <br/><br/>
+
+  ## User Story
+
+```
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+```
+
+  ## Software License
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) <br/>
+  This software is licensed under the "The MIT License" license.
+  https://opensource.org/licenses/MIT <br/><br/>
+
+  ## Contributors
+  Rich Stanbaugh<br/>
+  MSU Bootcamp Staff <br/><br/>
+
+  ## Testing
+  ✏️
+  `npm install jest --save-dev` <br/>
+
+  ## Questions
+  If you  hvae questions, you can:
+  - :eyes: Find me on GitHub: [rstanbaugh@gmail.com](https://github.com/rstanbaugh@gmail.com), or<br />
+  - :email: Email me: rich_bootcamp@icloud.com<br /><br />
+
+  _This README was generated with :heart: by [README-generator](https://github.com/rstanbaugh/README-Generator) :fire: :fire:_
+
+
---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,57 @@
+const { isNote, createNewNote, validateNote, deleteNote } = require('../../lib/notes');
+const { notes } = require('../../db/notes');
+
+const router = require('express').Router();
+
+// note api routes
+router.get('/notes', (req, res) => {
+  let results = notes;
+  // if (req.query){
+  //   results = filterByQuery(req.query, results);
+  // }
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = isNote(req.params.id, notes);
+  if(result){
+    res.json(result);
+  } else {
+    req.setEncoding(404);
+  }
+});
+
+router.post('/notes', (req, res) => {
+  // set an id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  // validate note
+  if(!validateNote(req.body)){
+    // console.log('not validating properly');
+    res.status(400).send('The note is not properly formatted');
+  } else {
+    // add note to json file and notes array
+    const note = createNewNote(req.body, notes);
+    res.json(req.body);
+  }
+});
+
+router.delete('/notes/:id', (req, res) => {
+  console.log('===================');
+  console.log('-->router.delete');
+  // make sure target note exists
+  if(isNote(req.params.id, notes)){
+    // delete it
+    // const note = deleteNote(req.params.id.replace(':',''), notes)
+    const note = deleteNote(req.params.id, notes)
+  } else {
+    let err = '404: did not find note w id = '+req.params.id;
+    console.log(err);
+    res.status(404).send(err)
+    return err;
+  }
+  console.log('<-- router.delete returning value(note):',notes)
+  //  delete a note from the json file and teh notes array
+  res.json(notes);
+});
+
+module.exports = router;
